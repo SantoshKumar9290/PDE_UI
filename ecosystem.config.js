dce09869a49d4f60b8ec682ed334acb1b1132d07
santosh <103992210+SantoshKumar9290@users.noreply.github.com>
2026-01-06 18:19:35 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,20 @@ module.exports = {
   apps: [
     {
       name: "pde_frontend",
-      script: "node_modules/.bin/serve",
-      args: "-s /opt/apps/frontend/current -l 3000",
-      exec_mode: "fork",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      cwd: "/opt/apps/frontend/current",
       env: {
-        NODE_ENV: "production"
+        NODE_ENV: "production",
+
+        BACKEND_URL: "http://10.10.120.190:8080",
+        PAYMENT_URL: "http://payment-url",
+        EC_API_URL: "http://ec-api-url",
+        TRANSFER_DOC_URL: "http://transfer-doc-url",
+        SLOT_BOOKING_URL: "http://slot-booking-url",
+        AADHAR_URL: "http://aadhar-url",
+        PASSPORT_URL: "http://passport-url",
+        PAN_URL: "http://pan-url"
       }
     }
   ]
